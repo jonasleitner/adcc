b913264488a203e061bc100c0b38deea7fcd355c
Jonas Leitner <j.leitner@stud.uni-heidelberg.de>
2021-05-09 09:55:10 +0200

    lr_pcm. should work

--- adcc/OperatorIntegrals.py
+++ adcc/OperatorIntegrals.py
@@ -102,7 +102,8 @@ class OperatorIntegrals:
             "electric_dipole",
             "magnetic_dipole",
             "nabla",
-            "pe_induction_elec"
+            "pe_induction_elec",
+            "pcm_induction_elec"
         )
         return [integral for integral in integrals
                 if hasattr(self.provider_ao, integral)]
@@ -185,6 +186,19 @@ class OperatorIntegrals:
                                       "not implemented "
                                       f"in {self.provider_ao.backend} backend.")
         callback = self.provider_ao.pe_induction_elec
+        return self.__import_density_dependent_operator(callback)
+
+    @property
+    def pcm_induction_elec(self):
+        """
+        Returns a function to obtain the (density-dependent)
+        PCM electronic induction operator in the molecular orbital basis
+        """
+        if "pcm_induction_elec" not in self.available:
+            raise NotImplementedError("PCM electronic induction operator "
+                                      "not implemented "
+                                      f"in {self.provider_ao.backend} backend.")
+        callback = self.provider_ao.pcm_induction_elec
         return self.__import_density_dependent_operator(callback)
 
     @property
